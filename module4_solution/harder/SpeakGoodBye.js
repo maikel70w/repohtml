bb77c4f3e7cc87899aee11b8aaf02ef9720d3250
maikel70w <maikel_70w@yahoo.es>
2022-06-02 18:17:58 -0400

    commit

--- module4_solution/harder/SpeakGoodBye.js
+++ module4_solution/harder/SpeakGoodBye.js
@@ -1,6 +1,6 @@
 // NOTE! The steps in this file are basically identical to the ones you
 // performed in the SpeakHello.js file.
-(function (windows) { 
+(function () { 
 
   // STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
 // See Lecture 52, part 2
@@ -19,9 +19,9 @@ var speakWord = "Good Bye";
 // See Lecture 52, part 2
 function speak(name) {
   console.log(speakWord + " " + name);
-}
+};
 
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
  windows.byeSpeaker = byeSpeaker;
-}); (window);
+}) ();
--- module4_solution/harder/SpeakHello.js
+++ module4_solution/harder/SpeakHello.js
@@ -1,6 +1,6 @@
 // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
 // See Lecture 52, part 2
-(function  (window) { 
+(function  () { 
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
@@ -14,7 +14,7 @@ var speakWord = "Hello";
 // See Lecture 52, part 2
 function speak(name) {
   console.log(speakWord + " " + name);
-}
+};
 
 // STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
 // 'helloSpeaker' on the global scope as well.
@@ -23,5 +23,5 @@ function speak(name) {
 windows.byeSpeaker = helloSpeaker;
 
 
-}); (window);
+}) ();
 
