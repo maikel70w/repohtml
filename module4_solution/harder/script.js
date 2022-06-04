3561415948a6b528961a7712267df1c8ea70c3ee
maikel70w <maikel_70w@yahoo.es>
2022-06-03 23:00:28 -0400

    commit

--- module4_solution/harder/script.js
+++ module4_solution/harder/script.js
@@ -32,6 +32,10 @@ WARNING!!! WARNING!!!
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 (function () {
+  var speakWord = "Hello";
+  function speak (name) {
+    console.log(speakWord + "" + name);
+  }
 
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
   
